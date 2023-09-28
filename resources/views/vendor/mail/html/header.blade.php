28de673509150dbf54bda3fd5c9f5ca3e49db40f
Ajay Nayak <connectusdemo12@gmail.com>
2023-09-28 14:13:26 +0530

    mail image replace text

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,16 @@
 <tr>
   <td class="header">
     <a href="{{ $url }}" style="display: inline-block;">
-      @if ($settings['logo'] ?? null)
+      <?php 
+	  /*
+	  @if ($settings['logo'] ?? null)
         <img src="{{ asset($settings['logo']) }}" class="logo" alt="Laravel Logo">
       @else
         {{ $slot }}
       @endif
+	  */
+	  ?>
+		<h3>Emerald Isle Homes</h3>
     </a>
   </td>
 </tr>
